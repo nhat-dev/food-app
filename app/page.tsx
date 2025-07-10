c07fa274825d932793dc220c1acc5edd80f1b340
Nhat <nhat.cse2022@gmail.com>
2025-07-10 15:09:46 +0700

    Add button for Vercel deployment in Home component of page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,16 @@ export default function Home() {
             />
             Deploy now
           </a>
+          <button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto">
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
